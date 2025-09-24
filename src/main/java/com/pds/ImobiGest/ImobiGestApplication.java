5c2e8239ea4b924b98dcdd3bfac9a0bf70c5e91f
ArthurLopes191 <arthurlopesreal@gmail.com>
2025-09-24 11:53:42 -0300

    Commit inicial

---
+++ src/main/java/com/pds/ImobiGest/ImobiGestApplication.java
@@ -0,0 +1,13 @@
+package com.pds.ImobiGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiGestApplication.class, args);
+	}
+
+}
